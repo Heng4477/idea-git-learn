4c5bfee44a3b7b5266098868e24a231fa5d8ea12
Lion <779386923@qq.com>
2022-03-02 13:31:42 +0800

    初次提交

---
+++ src/main/java/xyz/lion/git/GitTest.java
@@ -0,0 +1,13 @@
+package xyz.lion.git;
+
+/**
+ * @author ZhaoHeng
+ * @version V1.0
+ * @date Created in 13:28 2022/3/2
+ * @description Git学习
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
