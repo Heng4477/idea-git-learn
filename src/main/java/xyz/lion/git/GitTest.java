04d71cabb254b1f90b6eba8621bca1ec1c4920a5
Lion <779386923@qq.com>
2022-03-02 13:49:27 +0800

    hot-fix提交2

--- src/main/java/xyz/lion/git/GitTest.java
+++ src/main/java/xyz/lion/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello2");
         System.out.println("hello33");
         System.out.println("hot-fix");
+        System.out.println("hot-fix");
     }
 }
